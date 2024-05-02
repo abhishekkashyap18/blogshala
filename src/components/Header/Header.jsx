247fc27be20eef3fafc237d1b27a0fdc002a16d2
abhishekkashyap18 <117872546+abhishekkashyap18@users.noreply.github.com>
2024-05-02 13:41:48 +0530

    fixing responsiveness of navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,58 +1,61 @@
-import React from 'react';
-import {Container , Logo , LogoutBtn} from '../index'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
 // import {useSelector} from 'react-router-dom'
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
-  const authStatus = useSelector((state) => state.auth.status)
+  const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
 
   const navItems = [
     {
-      name: 'Home',
+      name: "Home",
       slug: "/",
-      active: true
-    }, 
+      active: true,
+    },
     {
-      name: 'Login',
+      name: "Login",
       slug: "/login",
-      active: !authStatus
+      active: !authStatus,
     },
     {
-      name: 'Signup',
+      name: "Signup",
       slug: "/signup",
-      active: !authStatus
+      active: !authStatus,
     },
     {
-      name: 'All Posts',
+      name: "All Posts",
       slug: "/all-posts",
-      active: authStatus
+      active: authStatus,
     },
     {
-      name: 'Add Post',
+      name: "Add Post",
       slug: "/add-post",
-      active: authStatus
-    }
-  ]
+      active: authStatus,
+    },
+  ];
 
   return (
-    <header className=' py-3 shadow bg-gray-500'>
+    <header className=" py-3 shadow bg-gray-500">
       <Container>
-        <nav className='flex'>
-          <div className='mr-4'>
-            <Link to='/'>
-              <Logo width='70px'/>
+        <nav className="flex">
+          <div className="mr-4 hidden sm:flex">
+            <Link to="/">
+              <Logo width="70px" />
             </Link>
           </div>
-          <ul className=' flex ml-auto'>
-            {navItems.map((item)=>
+          <ul className=" flex ml-auto">
+            {navItems.map((item) =>
               item.active ? (
                 <li key={item.name}>
                   <button
-                  onClick={()=>navigate(item.slug)}
-                  className=' inline-block sm:px-6 sm:py-2 duration-200 hover:bg-blue-100 rounded-full px-1 py-1'>{item.name}</button>
+                    onClick={() => navigate(item.slug)}
+                    className=" inline-block sm:px-6 sm:py-2 duration-200 hover:bg-blue-100 rounded-full px-4 py-2"
+                  >
+                    {item.name}
+                  </button>
                 </li>
               ) : null
             )}
