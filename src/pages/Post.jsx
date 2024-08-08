218fd85dd959f0c523435883a135c65d70dd0b2d
abhishekkashyap18 <117872546+abhishekkashyap18@users.noreply.github.com>
2024-08-08 12:11:26 +0530

    fix post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -69,10 +69,10 @@ export default function Post() {
                         </div>
                     )}
                 </div>
-                <div className="w-full mb-6">
+                <div className="w-full mb-6 text-white text-center">
                     <h1 className="text-2xl font-bold">{post.title}</h1>
                 </div>
-                <div className="browser-css">
+                <div className="browser-css text-white">
                     {parse(post.content)}
                     </div>
             </Container>
